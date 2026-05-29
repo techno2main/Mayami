9c3ef3b632994476ffdd46f69d545f3b8302526c
techno2main <techno2main@gmail.com>
2026-05-29 12:41:18 +0200

    fix: mettre à jour les liens de musique dans les fichiers de configuration

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,8 +1,8 @@
-export const SPOTIFY_LINK = "https://open.spotify.com/intl-fr/artist/2c6x9IL7EvoUU6XQ642S8c";
-export const APPLE_MUSIC_LINK = "https://music.apple.com/fr/album/music/1722440356";
-export const DEEZER_LINK = "https://www.deezer.com/fr/artist/5316718";
-export const YOUTUBE_MUSIC_LINK = "https://www.youtube.com/user/ellenemasriOFFICIAL";
-export const AMAZON_LINK = "https://music.amazon.fr/artists/B00GBFZTHW/ellene-masri";
+export const SPOTIFY_LINK = "https://open.spotify.com/intl-fr/track/3rzrziofCOwRrI1r99IUbQ?si=a2cd3f4cbe364a94";
+export const APPLE_MUSIC_LINK = "https://music.apple.com/fr/song/mayami-my-miami/6771742499";
+export const DEEZER_LINK = "https://link.deezer.com/s/33p3MydevJFz4yqu2aEam";
+export const YOUTUBE_MUSIC_LINK = "https://youtu.be/EH_QcQ92hSk?si=gpybhKJbZrDN1Ew5";
+export const AMAZON_LINK = "https://music.amazon.com/tracks/B0H2FR3WHQ?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_gPJPR79AtgfLS0EFarS9Xwi57";
 export const SOUNDCLOUD_LINK = "https://soundcloud.com/ellenemasri";
 export const YOUTUBE_VIDEO_LINK = "https://www.youtube.com/watch?v=WiB_UoexqVo&pp=0gcJCQoLAYcqIYzv";
 export const TIKTOK_LINK = "https://www.tiktok.com/@ellenemasri";
